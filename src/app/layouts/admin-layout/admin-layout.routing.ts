d47ef740fec4231c350f6176bf4fadc46a21562c
Raees-Ahamed <49068534+Raees-Ahamed@users.noreply.github.com>
2021-12-09 19:44:56 +0530

    created inventory table

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { InventoryViewComponent } from 'app/modules/inventory-management/components/inventory-view/inventory-view.component';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'inventory', component: InventoryViewComponent }
 ];
