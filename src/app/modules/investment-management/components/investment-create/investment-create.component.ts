e879574c04ceae2a9794220c1c9965b640237950
Roshan Nizar <roshannizar8@gmail.com>
2021-12-14 13:33:52 +0530

    added investment features

--- src/app/modules/investment-management/components/investment-create/investment-create.component.ts
+++ src/app/modules/investment-management/components/investment-create/investment-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { InvestmentModel } from '../../models/investment-model';
+import { InvestmentService } from '../../services/investment.service';
+import { InvestmentViewComponent } from '../investment-view/investment-view.component';
 
 @Component({
   selector: 'app-investment-create',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvestmentCreateComponent implements OnInit {
 
-  constructor() { }
+  isBlock = false;
+
+  investmentGroup: FormGroup;
+  investment: InvestmentModel;
+
+  constructor(public dialogRef: MatDialogRef<InvestmentViewComponent>, private fb: FormBuilder,
+    private investmentService: InvestmentService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.createInvestment();
   }
 
+  createInvestment() {
+    this.investmentGroup = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.email],
+      contactno: ['', Validators.required],
+      amount: [0, Validators.required]
+    })
+  }
+
+  onSave() {
+    this.isBlock = true;
+    this.investment = Object.assign({}, this.investment, this.investmentGroup.value);
+    this.investmentService.createInvestment(this.investment).subscribe(
+      (result) => {
+        this.investmentGroup.reset();
+        this.isBlock = false;
+        this.close();
+        this.toastr.success('Investment created successfully!', 'Success');
+      },
+      (error) => {
+        this.isBlock = false;
+        this.toastr.error(error.message, 'Failed to created invesmtnet');
+      }
+    );
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
