aee34673a1fb1051b7d098e0e779b101f03047ab
Roshan Nizar <roshannizar8@gmail.com>
2022-02-11 00:05:30 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  endpointUrl: 'https://mingems-api.herokuapp.com/api',
+  endpointUrl: 'https://mingems-api-staging.herokuapp.com/api',
   signalrRUrl: 'https://placemeapi.herokuapp.com/order',
   cloudStorageUrl: 'https://mingems-cloud.herokuapp.com/api',
   production: true
